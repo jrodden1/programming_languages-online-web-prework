4b5524ea5d02d7b957d0f2eb203ffc6d92a67962
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-04 16:14:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,8 +4,8 @@ def reformat_languages(languages)
   languages.each do |style, langhash|
     langhash.each do |langName, typehash|
       typehash.each do |type, value|
-        new_hash[":"+langName][":"+type] = value
-        new_hash[":"+langName][":"+style] = style
+        new_hash[langName][type] = value
+        new_hash[langName][style] = style
       end
     end
   end
