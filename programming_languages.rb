5f561ee9f000ce2c1f7b2823b74668cefd484027
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-04 16:37:27 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,9 +7,9 @@ def reformat_languages(languages)
     langhash.each do |langName, typehash|
       typehash.each do |type, value|
         new_hash[langName] = {type => value}
-        binding.pry
+        # binding.pry
         if new_hash[langName].has_key?(:style) == false
-          new_hash[langName][:style] = [style]}
+          new_hash[langName][:style] = [style]
         else
           new_hash[langName][:style] = new_hash[langName][:style] << style
         end
